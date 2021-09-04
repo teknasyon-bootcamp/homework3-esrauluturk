8620cafe77490f96411cd36bb26b76c30e7f8e94
Esra Ulutürk <73850856+esrauluturk@users.noreply.github.com>
2021-09-04 20:48:48 +0300

    Update form.php

--- form.php
+++ form.php
@@ -24,3 +24,53 @@
  * 
  * > **Not: İsteyenler `app2.php` ve `form2.php` isminde dosyalar oluşturup sınıfa farklı özellikler kazandırabilir.
  */
+class Form{ //Form sinifi 
+    //Property tanimlama
+    private array $fields=[];
+    //Private construct olusturma
+    //Otomatik olarak nesnenin propertysine yerlestirilir.(php8)
+    private function __construct(
+        private string $action,
+        private string $method)//Turetilirken zorunlu olan degerler
+    { }
+    //Static olarak form nesnesi donduren(return) methodlar
+    //Tekil olmasini istersek static kullaniriz.
+    //Nesne uretmeye gerek kalmadan method ve ozellikler kullanilabilir.
+    public static function createPostForm(string $action):Form{
+        return new static($action,"POST");//static hangi class cagirdiysa onu temsil eder.
+    }
+    public static function createGetForm(string $action): Form{
+        return new static($action,"GET");
+    }
+    //Form olusturan fonk.
+    public static function createForm(string $action,string $method):Form{
+        return new static($action,$method);
+    }
+    //Nonstatic Void Methodlar
+    //fields property dizisine deger ekler.
+    public function addField(string $label, string $name, ?string $defaultValue=null):void{
+        $field["label"]=$label;
+        $field["name"]=$name;
+        $field["defaultValue"]=$defaultValue;
+        array_push($this->fields, $field);
+    }
+    //method propertysinin degerini degistirir.
+    public function setMethod(string $method):void{
+        $this-> method=$method;
+    }
+    //formun ilgili alanlarini HTML cikti olarak verir.
+    public function render():void{
+        echo "<form action='".$this->action."' method='".$this->method."'>";
+        foreach($this->fields as $field){
+            echo "\t<label for='".$field["label"]."'>".$field["label"]."</label>";
+            if(isset($field["defaultValue"])){
+                echo "\t<input type='text' name='".$field["name"]."' value='".$field["defaultValue"]."'/>";
+            }
+            else{
+                echo "\t<input type='text' name='".$field["name"]."'/>";
+            }
+        }
+        echo "\t<button type='submit'>Gönder</button>";
+        echo "</form>";
+    }
+}
